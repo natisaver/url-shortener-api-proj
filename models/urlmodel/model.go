18a6ef6e6636985b451ca2f0feac94a26688b4e4
nathaniel <64686163+natisaver@users.noreply.github.com>
2024-03-20 17:28:17 +0800

    Update model.go

--- models/urlmodel/model.go
+++ models/urlmodel/model.go
@@ -13,4 +13,9 @@ type URL struct {
 	LongURL   string `json:"longurl" gorm:"column:longurl"`
 	CreatedAt uint64 `json:"createdat" gorm:"column:createdat"`
 	// store time as uint64 as a standard, unsigned long integer without timezone, do processing outside when retrieving
+	// so in DB to store as long its called BIGINT
+	// WHY uint64?
+	// 1. standardised, we usually do to milliseconds
+	// 2. numbers are easier to index, processing is faster
+	// 3. smaller space usage compared to storing a composite, using a primitive type like long/BIGINT (8 bytes) will have a dedicated chunk allocated, compared to composite types that will dedicate a much larger chunk (do note this is different from varchar which has 255 bytes allocated)
 }
